f0836f54ddfc045eca5bc09e21112eb55066bb0c
Bhaskar <bhaskar26.pawar@gmail.com>
2019-07-07 19:08:29 +0530

    apidoc-commit

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Meeting Planner",
+  "version": "1.0.0",
+  "description": "Documentation for the meeting planner application",
+  "title": "Meeting Planner APIs",
+  "url": "http://planmeeting.in",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-07-07T13:36:04.739Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
